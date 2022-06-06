c8543c7704638d821c76ed60914a6e31d22bcc31
Q-bas <kubyk.a@gmail.com>
2022-06-06 18:55:33 -0500

    listeners

---
+++ src/test/java/tests/day4.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class day4 {
+    @Test
+    public void Test1_2() {
+        System.out.println("Day4_test1");
+        Assert.assertTrue(1==0);
+    }
+
+}
